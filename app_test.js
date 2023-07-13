f50776a473ea12642646959b40293614c0b1188a
Chris <christian.altenhofer@gmx.de>
2023-07-13 22:18:41 +0200

    verwirrt rofllol

--- app_test.js
+++ app_test.js
@@ -4,7 +4,57 @@ const questions = [
         answers: ['Rick', 'Popeye', 'Teletubby', 'Captain Hero'],
         correctAnswer: 'Rick'
     },
-    //...
+    {
+        question: 'Wer ist der coolste?!',
+        answers: ['Rick', 'Popeye', 'Teletubby', 'Captain Hero'],
+        correctAnswer: 'Rick'
+    },
+    {
+        question: 'Welche Farbe hat die Black Order?',
+        answers: ['Blau', 'Grau', 'Lila', 'Schwarz'],
+        correctAnswer: 'Schwarz'
+    },
+    {
+        question: 'Wer ist Chef?',
+        answers: ['Garfield', 'Batman', 'Martin Dubb', 'Chefkoch'],
+        correctAnswer: 'Martin Dubb'
+    },
+    {
+        question: 'Ey man, wo ist mien Auto?',
+        answers: ['vor der Tür', 'Garage', 'Shaweet', 'Dude'],
+        correctAnswer: 'Dude'
+    },
+    {
+        question: 'Was wächst nicht?',
+        answers: ['Unkraut', 'Bohnenranken', 'Wachs', 'Fuss'],
+        correctAnswer: 'Wachs'
+    },
+    {
+        question: 'Wer war der letzte Kaiser Deutschlands?',
+        answers: ['Bismarck', 'Beckenbauer', 'Franz', 'Wilhelm'],
+        correctAnswer: 'Wilhelm'
+    },
+    {
+        question: 'Wer ist der treuste Begleiter?',
+        answers: ['Snoopy', 'Morty', 'Robin', 'Donald Trump'],
+        correctAnswer: 'Morty'
+    },
+    {
+        question: 'Wo brennts?',
+        answers: ['In der Hütte', 'Im Wasser', 'Am Hintern', 'Im Ofen'],
+        correctAnswer: 'Im Ofen'
+    },
+    {
+        question: 'Warum mach ich das?',
+        answers: ['*Affe der sich die Augen zuhält*', 'Weil ichs kann!', 'Weiß ich selbst net...', 'Nächste Frage bitte!'],
+        correctAnswer: 'Nächste Frage bitte!'
+    },
+    {
+        question: 'Na wie war das Quiz?',
+        answers: ['Welches Quiz?', 'Bahnhof', 'Lass einfach sein jetzt!', 'Fahrrad'],
+        correctAnswer: 'Lass einfach sein jetzt!'
+    }
+    
 ];
 
 const questionElement = document.getElementById("question-box");
