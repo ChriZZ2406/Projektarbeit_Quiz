3e4aa2a1e13d36e5eea8cad3e55fae3806ac439c
Flo <florian.britz@docc.techstarter.de>
2023-07-18 09:30:16 +0200

    neuer Verusch mit Anleitung W3 von Martin (noch nicht funktionsfähig)

--- app.js
+++ app.js
@@ -1,9 +1,399 @@
+/*
 const questions = [
+     {
+        question: 'Wer ist der coolste?!',
+        answers: [
+            { text: "Rick", correct: true},
+            { text: "Popeye", correct: false},
+            { text: "Teletubby", correct: false},
+            { text: "Captain Hero", correct: false},
+        ]
+    },
+    {
+        question: 'Welche Farbe hat die Black Order?',
+        answers: [
+            { text: "Blau", correct: false },
+            { text: "Grau", correct: false },
+            { text: "Lila", correct: false },
+            { text: "Schwarz", correct: true }
+        ]
+    },
+    {
+        question: 'Wer ist Chef?',
+        answers: [
+            { text: "Garfield", correct: false },
+            { text: "Batman", correct: false },
+            { text: "Martin Dubb", correct: true },
+            { text: "Chefkoch", correct: false }
+        ]
+    },
+    {
+        question: 'Ey man, wo ist mien Auto?',
+        answers: [
+            { text: "vor der Tür", correct: false },
+            { text: "Garage", correct: false },
+            { text: "Shaweet", correct: false },
+            { text: "Dude", correct: true }
+        ]
+    },
+    {
+        question: 'Was wächst nicht?',
+        answers: [
+            { text: "Unkraut", correct: false },
+            { text: "Bohnenranken", correct: false },
+            { text: "Wachs", correct: true },
+            { text: "Fuss", correct: false }
+        ]
+    },
+    {
+        question: 'Wer war der letzte Kaiser Deutschlands?',
+        answers: [
+            { text: "Bismarck", correct: false },
+            { text: "Beckenbauer", correct: false },
+            { text: "Franz", correct: false },
+            { text: "Wilhelm", correct: true }
+        ]
+    },
+    {
+        question: 'Wer ist der treuste Begleiter?',
+        answers: [
+            { text: "Snoopy", correct: false },
+            { text: "Morty", correct: true },
+            { text: "Robin", correct: false },
+            { text: "Donald Trump", correct: false }
+        ]
+    },
+    {
+        question: 'Wo brennts?',
+        answers: [
+            { text: "In der Hütte", correct: false },
+            { text: "Im Wasser", correct: false },
+            { text: "Am Hintern", correct: false },
+            { text: "Im Ofen", correct: true }
+        ]
+    },
+    {
+        question: 'Warum mach ich das?',
+        answers: [
+            { text: "*Affe der sich die Augen zuhält*", correct: false },
+            { text: "Weil ichs kann!", correct: false },
+            { text: "Weiß ich selbst net...", correct: false },
+            { text: "Nächste Frage bitte!", correct: true }
+        ]
+    },
+    {
+        question: 'Na wie war das Quiz?',
+        answers: [
+            { text: "Welches Quiz?", correct: false },
+            { text: "Bahnhof", correct: false },
+            { text: "Lass einfach sein jetzt!", correct: true },
+            { text: "Fahrrad", correct: false }
+        ]
+    }
+  ];
+  
+  const questionElement = document.getElementById("question-box");
+  const answerButtonsElement = document.getElementById("answer-buttons");
+  const timerElement = document.getElementById("timer");
+  const nextButton = document.getElementById("next-button");
+  
+  let shuffledQuestions, currentQuestionIndex;
+  let score = 0;
+  let countdown;
+  
+  function startGame() {
+    score = 0;
+    currentQuestionIndex = 0;
+    nextButton.innerHTML = "Nächste Frage";
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+  }
+  
+  function startTimer() {
+    let timeLeft = 15;
+    timerElement.innerText = `${timeLeft}`;
+    countdown = setInterval(() => {
+      timeLeft--;
+      timerElement.innerText = `${timeLeft}`;
+      if(timeLeft <= 0) {
+          clearInterval(countdown);
+          Array.from(answerButtonsElement.children).forEach(button => {
+              if (button.dataset.correct) selectAnswer({ target: button });
+          });
+      }
+    }, 1000);
+  }
+  
+  function setNextQuestion() {
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+  }
+  
+  function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+      const button = document.createElement('button');
+      button.innerText = answer.text;
+      button.classList.add('btn');
+      if (answer.correct) {
+        button.dataset.correct = answer.correct;
+      }
+      button.addEventListener('click', selectAnswer);
+      answerButtonsElement.appendChild(button);
+    });
+    startTimer();
+  }
+  
+  function selectAnswer(e) {
+    clearInterval(countdown);
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonsElement.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct);
+    });
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+      nextButton.classList.remove('hide');
+    } else {
+      nextButton.innerText = 'Neustart';
+      nextButton.classList.remove('hide');
+    }
+  }
+  
+  function resetState() {
+    nextButton.classList.add('hide');
+    while (answerButtonsElement.firstChild) {
+      answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+    }
+  }
+  
+  function showScore() {
+    questionElement.innerText = 'Du hast ' + score + ' von ' + questions.length + ' möglichen Punkten!';
+    nextButton.innerText = "Neustart";
+    nextButton.classList.remove('hide');
+  }
+  
+  function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+      element.classList.add('correct');
+    } else {
+      element.classList.add('incorrect');
+    }
+  }
+  
+  function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('incorrect');
+  }
+  
+  nextButton.addEventListener("click", () => {
+    currentQuestionIndex++;
+    setNextQuestion();
+  });
+  
+  startGame();
+
+
+  const questions = [
     {
         question: 'Wer ist der coolste?!',
-        answers: ['Rick', 'Popeye', 'Teletubby', 'Captain Hero'],
-        correctAnswer: 'Rick'
+        answers: [
+            { text: "Rick", correct: true},
+            { text: "Popeye", correct: false},
+            { text: "Teletubby", correct: false},
+            { text: "Captain Hero", correct: false},
+        ]
     },
+    {
+        question: 'Welche Farbe hat die Black Order?',
+        answers: [
+            { text: "Blau", correct: false },
+            { text: "Grau", correct: false },
+            { text: "Lila", correct: false },
+            { text: "Schwarz", correct: true }
+        ]
+    },
+    {
+        question: 'Wer ist Chef?',
+        answers: [
+            { text: "Garfield", correct: false },
+            { text: "Batman", correct: false },
+            { text: "Martin Dubb", correct: true },
+            { text: "Chefkoch", correct: false }
+        ]
+    },
+    {
+        question: 'Ey man, wo ist mien Auto?',
+        answers: [
+            { text: "vor der Tür", correct: false },
+            { text: "Garage", correct: false },
+            { text: "Shaweet", correct: false },
+            { text: "Dude", correct: true }
+        ]
+    },
+    {
+        question: 'Was wächst nicht?',
+        answers: [
+            { text: "Unkraut", correct: false },
+            { text: "Bohnenranken", correct: false },
+            { text: "Wachs", correct: true },
+            { text: "Fuss", correct: false }
+        ]
+    },
+    {
+        question: 'Wer war der letzte Kaiser Deutschlands?',
+        answers: [
+            { text: "Bismarck", correct: false },
+            { text: "Beckenbauer", correct: false },
+            { text: "Franz", correct: false },
+            { text: "Wilhelm", correct: true }
+        ]
+    },
+    {
+        question: 'Wer ist der treuste Begleiter?',
+        answers: [
+            { text: "Snoopy", correct: false },
+            { text: "Morty", correct: true },
+            { text: "Robin", correct: false },
+            { text: "Donald Trump", correct: false }
+        ]
+    },
+    {
+        question: 'Wo brennts?',
+        answers: [
+            { text: "In der Hütte", correct: false },
+            { text: "Im Wasser", correct: false },
+            { text: "Am Hintern", correct: false },
+            { text: "Im Ofen", correct: true }
+        ]
+    },
+    {
+        question: 'Warum mach ich das?',
+        answers: [
+            { text: "*Affe der sich die Augen zuhält*", correct: false },
+            { text: "Weil ichs kann!", correct: false },
+            { text: "Weiß ich selbst net...", correct: false },
+            { text: "Nächste Frage bitte!", correct: true }
+        ]
+    },
+    {
+        question: 'Na wie war das Quiz?',
+        answers: [
+            { text: "Welches Quiz?", correct: false },
+            { text: "Bahnhof", correct: false },
+            { text: "Lass einfach sein jetzt!", correct: true },
+            { text: "Fahrrad", correct: false }
+        ]
+    }
+  ];
+  
+  const questionElement = document.getElementById("question-box");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const timerElement = document.getElementById("timer");
+const startButton = document.getElementById("start-button");
+const startButtonText = document.getElementById("start-button-text");
+
+let shuffledQuestions, currentQuestionIndex;
+let score = 0;
+let countdown;
+
+startButton.addEventListener('click', () => {
+    startGame();
+    startButton.style.visibility = 'hidden';
+    startButtonText.style.visibility = 'hidden';
+});
+
+function startGame() {
+    score = 0;
+    currentQuestionIndex = 0;
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function startTimer() {
+    let timeLeft = 15;
+    timerElement.innerText = `${timeLeft}`;
+    countdown = setInterval(() => {
+        timeLeft--;
+        timerElement.innerText = `${timeLeft}`;
+        if(timeLeft <= 0) {
+            clearInterval(countdown);
+            timerElement.innerText = 'Zeit abgelaufen!';
+            Array.from(answerButtonsElement.children).forEach(button => {
+                button.disabled = true;
+            });
+        }
+    }, 1000);
+}
+
+function setNextQuestion() {
+    resetState();
+    currentQuestionIndex++;
+    if (currentQuestionIndex < shuffledQuestions.length) {
+        showQuestion(shuffledQuestions[currentQuestionIndex]);
+    } else {
+        showScore();
+        startButton.style.visibility = 'visible';
+        startButtonText.style.visibility = 'visible';
+        startButtonText.innerText = 'Neustart';
+    }
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach((answer, index) => {
+        const button = document.getElementById(`answer-${index + 1}`);
+        button.innerText = answer.text;
+        button.dataset.correct = answer.correct;
+        button.disabled = false;
+        button.addEventListener('click', selectAnswer);
+    });
+    startTimer();
+}
+
+function selectAnswer(e) {
+    clearInterval(countdown);
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(selectedButton, correct);
+    if (correct) {
+        score++;
+    }
+    setTimeout(setNextQuestion, 1000);
+}
+
+function resetState() {
+    clearInterval(countdown);
+    timerElement.innerText = "15";
+    Array.from(answerButtonsElement.children).forEach(button => {
+        button.innerText = "";
+        clearStatusClass(button);
+        button.disabled = true;
+        button.removeEventListener('click', selectAnswer);
+    });
+}
+
+function showScore() {
+    questionElement.innerText = 'Du hast ' + score + ' von ' + questions.length + ' möglichen Punkten!';
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('incorrect');
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('incorrect');
+}
+*/
+
+const questions = [
     {
         question: 'Wer ist der coolste?!',
         answers: ['Rick', 'Popeye', 'Teletubby', 'Captain Hero'],
@@ -20,7 +410,7 @@ const questions = [
         correctAnswer: 'Martin Dubb'
     },
     {
-        question: 'Ey man, wo ist mien Auto?',
+        question: 'Ey man, wo ist mein Auto?',
         answers: ['vor der Tür', 'Garage', 'Shaweet', 'Dude'],
         correctAnswer: 'Dude'
     },
@@ -47,93 +437,102 @@ const questions = [
     {
         question: 'Warum mach ich das?',
         answers: ['*Affe der sich die Augen zuhält*', 'Weil ichs kann!', 'Weiß ich selbst net...', 'Nächste Frage bitte!'],
-        correctAnswer: 'Nächste Frage bitte!'
+        correctAnswer: 'Weil ichs kann!'
     },
     {
-        question: 'Na wie war das Quiz?',
-        answers: ['Welches Quiz?', 'Bahnhof', 'Lass einfach sein jetzt!', 'Fahrrad'],
-        correctAnswer: 'Lass einfach sein jetzt!'
+        question: 'Na wie is das Quiz?',
+        answers: ['Welches Quiz?', 'Bahnhof', 'nope', 'Fahrrad'],
+        correctAnswer: 'Welches Quiz?'
     }
     
 ];
-
-const questionElement = document.getElementById("question-box");
-const answerButtonsElement = document.getElementById("answer-buttons");
-const timerElement = document.getElementById("timer");
-
-let shuffledQuestions, currentQuestionIndex;
-let score = 0;
-let countdown;
-
-function startGame() {
-    score = 0;
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
-    currentQuestionIndex = 0;
-    setNextQuestion();
-    startTimer();
-}
-
-function startTimer() {
-    let timeLeft = 15;
-    timerElement.innerText = `${timeLeft}`;
-    countdown = setInterval(() => {
-        timeLeft--;
-        timerElement.innerText = `${timeLeft}`;
-        if(timeLeft <= 0) {
-            clearInterval(countdown);
-            Array.from(answerButtonsElement.children).forEach(button => {
-                if (button.dataset.correct) selectAnswer({ target: button });
-            });
-        }
-    }, 1000);
-}
-
-function setNextQuestion() {
-    resetState();
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
-}
-
-function showQuestion(question) {
-    questionElement.innerText = question.question;
-    question.answers.forEach(answer => {
-        const button = document.createElement('button');
-        button.innerText = answer;
-        button.classList.add('btn');
-        if (answer === question.correctAnswer) {
-            button.dataset.correct = true;
-        }
-        button.addEventListener('click', selectAnswer);
-        answerButtonsElement.appendChild(button);
-    });
-}
-
-function selectAnswer(e) {
-    clearInterval(countdown);
-    let selectedButton;
-    if(e){
-        selectedButton = e.target;
-    } else {
-        selectedButton = answerButtonsElement.children[0];
-    }
-    const correct = selectedButton.dataset.correct;
-    if (correct) {
-        score++;
-        alert(`Richtig! Dein Punktestand ist jetzt ${score}.`);
-    } else {
-        alert(`Falsch! Dein Punktestand bleibt bei ${score}.`);
-    }
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-        currentQuestionIndex++;
-        setNextQuestion();
-    } else {
-        alert(`Quiz beendet! Dein endgültiger Punktestand ist ${score}.`);
-    }
-}
-
-function resetState() {
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
-    }
-}
-
-startGame();
+  
+  const questionElement = document.getElementById("question-box");
+  const answerButtonsElement = document.getElementById("answer-buttons");
+  const timerElement = document.getElementById("timer");
+  
+  let shuffledQuestions, currentQuestionIndex;
+  let score = 0;
+  let countdown;
+  
+  function startGame() {
+      score = 0;
+      shuffledQuestions = questions.sort(() => Math.random() - .5);
+      currentQuestionIndex = 0;
+      setNextQuestion();
+      startTimer();
+  }
+  
+  function startTimer() {
+      let timeLeft = 15;
+      timerElement.innerText = `${timeLeft}`;
+      countdown = setInterval(() => {
+          timeLeft--;
+          timerElement.innerText = `${timeLeft}`;
+          if(timeLeft <= 0) {
+              clearInterval(countdown);
+              Array.from(answerButtonsElement.children).forEach(button => {
+                  if (button.dataset.correct) selectAnswer({ target: button });
+              });
+          }
+      }, 1000);
+  }
+  
+  function setNextQuestion() {
+      resetState();
+      showQuestion(shuffledQuestions[currentQuestionIndex]);
+  }
+  
+  function showQuestion(question) {
+      questionElement.innerText = question.question;
+      question.answers.forEach(answer => {
+          const button = document.createElement('button');
+          button.innerText = answer;
+          button.classList.add('btn');
+          if (answer === question.correctAnswer) {
+              button.dataset.correct = true;
+          }
+          button.addEventListener('click', selectAnswer);
+          answerButtonsElement.appendChild(button);
+      });
+  }
+  
+  function selectAnswer(e) {
+      clearInterval(countdown);
+      let selectedButton;
+      if(e){
+          selectedButton = e.target;
+      } else {
+          selectedButton = answerButtonsElement.children[0];
+      }
+      const correct = selectedButton.dataset.correct;
+      if (correct) {
+          score++;
+          alert(`Richtig! Dein Punktestand ist jetzt ${score}.`);
+          if (shuffledQuestions.length > currentQuestionIndex + 1) {
+              currentQuestionIndex++;
+              setNextQuestion();
+              startTimer();  // Timer zurücksetzen, wenn die Antwort korrekt ist
+          } else {
+              alert(`Quiz beendet! Dein endgültiger Punktestand ist ${score}.`);
+          }
+      } else {
+          alert(`Falsch! Dein Punktestand bleibt bei ${score}.`);
+          if (shuffledQuestions.length > currentQuestionIndex + 1) {
+              currentQuestionIndex++;
+              setNextQuestion();
+              startTimer();  // Timer zurücksetzen, wenn die Antwort falsch ist
+          } else {
+              alert(`Quiz beendet! Dein endgültiger Punktestand ist ${score}.`);
+          }
+      }
+  }
+  
+  function resetState() {
+      while (answerButtonsElement.firstChild) {
+          answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+      }
+  }
+  
+  startGame();
+  
